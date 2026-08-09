4463861adece92e9a2b55695fd913967bbf2a9d8
josh <jb3152@student.ohs.school.nz>
2026-08-09 14:46:04 +1200

    arm works somehow
    bro the claw needs to lock in only semis again

--- Ganz-Mavag/src/main.cpp
+++ Ganz-Mavag/src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
+#include "pros/adi.hpp"
 #include "pros/misc.h"
+#include "pros/motors.h"
 
 /**
  * A callback function for LLEMU's center button.
@@ -18,9 +20,13 @@ void on_center_button() {
 }
 pros::MotorGroup left_drive({-3, -4});     // Creates a motor group with reversed port 11 and reversed port 12
 pros::MotorGroup right_drive({1, 2});  // Creates a motor group with forward port 1 and forwards port 2
-pros::Motor arm_1(-7,pros::MotorGearset::green);
-pros::Motor arm_2(8,pros::MotorGearset::green);
+pros::Motor arm_1(-6,pros::MotorGearset::red);
+pros::Motor arm_2(8,pros::MotorGearset::red);
 pros::MotorGroup arm(arm_1);
+pros::Motor cascade_1(0,pros::MotorGearset::blue);
+pros::Motor cascade_2(0,pros::MotorGearset::blue);
+pros::MotorGroup cascade(cascade_1);
+pros::Motor claw(10,pros::MotorGearset::green);
 /**
  * Runs initialization code. This occurs as soon as the program is started.
  *
@@ -95,7 +101,8 @@ void autonomous() {
 void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	arm.append(arm_2);
-  
+	arm.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
+  	claw.set_brake_mode(pros::E_MOTOR_BRAKE_HOLD);
 	while (true) {
 		// pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
 		//                  (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
@@ -105,14 +112,27 @@ void opcontrol() {
 		left_drive.move(master.get_analog(ANALOG_LEFT_Y));           			 // Sets left motor voltage
 		right_drive.move(master.get_analog(ANALOG_RIGHT_Y));                     // Sets right motor voltage
 		if(master.get_digital(DIGITAL_R1)){
-			arm.move_velocity(100);
+			arm.move_velocity(30);
 		}
 		else if(master.get_digital(DIGITAL_R2)){
-			arm.move_velocity(-100);
+			arm.move_velocity(-30);
 		}
 		else{
 			arm.brake();
 		}
+
+		if(master.get_digital(DIGITAL_L1)){
+			claw.move_velocity(100);
+		}
+		else if(master.get_digital(DIGITAL_L2)){
+			claw.move_velocity(-50);
+		}
+		else{
+			claw.brake();
+		}
+		
+		
+		
 
 		pros::delay(20);                               // Run for 20 ms then update
 	}
